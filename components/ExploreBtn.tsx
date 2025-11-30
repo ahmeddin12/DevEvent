1649669ed5378f648d55caf088e57cfbc232a3dd
ahmeddin12 <amudi621@gmail.com>
2025-11-30 22:05:13 +0300

    Add ExploreBtn component to Home page for event navigation

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 
 const ExploreBtn = () => {
     return (
-        <button type='submit' className='mt-7 mx-auto' onClick={()=>console.log('clicked')}>
-            <a href='#events'>Explore Events
-            <Image src='./icons/arrow-down.svg' alt='arrow-down' width={24} height={24}/>
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
             </a>
         </button>
     )
