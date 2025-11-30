c951d0751d33c285a3731a56fd67cd071847d966
ahmeddin12 <amudi621@gmail.com>
2025-11-30 21:33:39 +0300

    Add ExploreBtn component to Home page for event navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ExploreBtn from "@/components/ExploreBtn";
 
 const Home = () => {
   return (
       <section>
           <h1 className='text-center'>The Hub for Every Dev<br />Event You Can't Miss</h1>
           <p className='text-center mt-5'>Hackathons, Meetups, and Conference, All in one Place</p>
+          <ExploreBtn />
       </section>
   )
 }
