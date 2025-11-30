bed2e801938e2f2df380d1eb151facfbcff38b72
ahmeddin12 <amudi621@gmail.com>
2025-11-30 22:05:58 +0300

    Display sample event list on the Home page.

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,14 @@ const Home = () => {
           <h1 className='text-center'>The Hub for Every Dev<br />Event You Can't Miss</h1>
           <p className='text-center mt-5'>Hackathons, Meetups, and Conference, All in one Place</p>
           <ExploreBtn />
+
+          <div className='mt-7 space-y-7'>
+              <ul className='events'>
+                  {[1, 2, 3, 4, 5].map((event)=>(
+                      <li>Event {event}</li>
+                  ))}
+              </ul>
+          </div>
       </section>
   )
 }
