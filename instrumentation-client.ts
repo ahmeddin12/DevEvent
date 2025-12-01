578bd89fc7462dcaab487254205e2f5d02cf9690
ahmeddin12 <amudi621@gmail.com>
2025-12-01 14:00:06 +0300

    Add conditional initialization for PostHog to prevent errors when the key is missing

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,10 +1,15 @@
 import posthog from "posthog-js"
 
 // Automatically initializes PostHog in the Next.js app and captures pageviews
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: "2025-05-24",
-  capture_exceptions: true, // Enable error tracking
-  debug: process.env.NODE_ENV === "development",
-});
+
+
+const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+if (posthogKey) {
+    posthog.init(posthogKey, {
+        api_host: "/ingest",
+        ui_host: "https://us.posthog.com",
+        defaults: "2025-05-24",
+        capture_exceptions: true, // Enable error tracking
+        debug: process.env.NODE_ENV === "development",
+    });
+}
