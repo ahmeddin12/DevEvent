03156d01abeae284679f47f938609a85c8610351
ahmeddin12 <amudi621@gmail.com>
2025-12-03 23:06:28 +0300

    refactored posthog for better initialization

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,15 +1,18 @@
-import posthog from "posthog-js"
+import { useEffect } from "react";
+import posthog from "posthog-js";
 
-// Automatically initializes PostHog in the Next.js app and captures pageviews
+const usePostHog = () => {
+    useEffect(() => {
+        const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+        if (posthogKey && typeof window !== "undefined" && !posthog.__loaded) {
+            posthog.init(posthogKey, {
+                api_host: "/ingest",
+                ui_host: "https://us.posthog.com",
+                capture_exceptions: true,
+                debug: process.env.NODE_ENV === "development",
+            });
+        }
+    }, []);
+};
 
-
-const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-if (posthogKey) {
-    posthog.init(posthogKey, {
-        api_host: "/ingest",
-        ui_host: "https://us.posthog.com",
-        defaults: "2025-05-24",
-        capture_exceptions: true, // Enable error tracking
-        debug: process.env.NODE_ENV === "development",
-    });
-}
+export default usePostHog;
