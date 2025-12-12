735b643d8c5913e96c6d0f2e0cab9421bfe32b76
ahmeddin12 <amudi621@gmail.com>
2025-12-12 22:37:16 +0300

    refactor base URL handling and improve event fetching logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,49 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {IEvent} from "@/database";
-import {cacheLife} from "next/cache";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
-const BASE_URL = 'https://dev-event-two-sigma.vercel.app/';
+function getBaseUrl() {
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL;
+  return "http://localhost:3000";
+}
 
 const Page = async () => {
-    'use cache';
-    cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const { events } = await response.json();
+  "use cache";
+  cacheLife("hours");
+  const base = getBaseUrl();
+  const response = await fetch(new URL("/api/events", base).toString(), {
+    next: { revalidate: 3600 },
+  });
+  const { events } = await response.json();
 
-    return (
-        <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
-            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
 
-            <ExploreBtn />
+      <ExploreBtn />
 
-            <div className="mt-20 space-y-7">
-                <h3>Featured Events</h3>
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
 
-                <ul className="events">
-                    {events && events.length > 0 && events.map((event: IEvent) => (
-                        <li key={event.title} className="list-none">
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    )
-}
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Page;
